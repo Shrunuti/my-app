919d9ceb5882b9c4eb877063355d952b0fe86325
Aishwarya Kulkarni <kulkarniaishwarya6@gmail.com>
2021-08-13 00:19:43 +0530

    github search & filter
    new funcionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {AppService} from './app.service';
 import { HttpClient } from '@angular/common/http';
 @Component({
@@ -7,75 +7,60 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app1';
-  year_buttons:any=[];
+  title = 'Github search repo';
+  
   defaultResponse:any =[];
-  mission_name:any="";
-  flight_number:any="";
-  mission_ids:any=[];
-  launch_year:any;
-  launch_success:any;
-  launch_landing:any;
+  username:any;
   dataBackup:any;
-  filterYear:any;
-  filterLaunch:any;
-  filterLand:any;
+  url:any;
   isDataEmpty:any;
+  srch_repo:any="";
+  srch_owner:any="";
+  srch_star:any;
+  srch_issuecount:any;
+  srch_watch:any;
+
   constructor(private appservice:AppService, private http:HttpClient){}
   
   ngOnInit(){
       console.log("welcom to app");
-      for(let i=6;i<=20;i++){
-        if(i<10){
-          this.year_buttons.push("200"+i);
-        }
-        else{
-          this.year_buttons.push("20"+i);
-        }
-      }
-      this.http.get<any>('https://api.spaceXdata.com/v3/launches?limit=100').subscribe(data => {
+     ///users/{username}/repos
+      
+  }
+  Submit(){
+    this.srch_issuecount="";
+    this.srch_owner="";
+    this.srch_repo;
+    this.srch_star;
+    this.srch_watch;
+    this.url = "https://api.github.com/users/"+this.username+"/repos";
+    this.http.get<any>(this.url).subscribe(data => {
         this.defaultResponse = data;
         this.dataBackup = data;
       });
   }
-  filterByYear(year:any){
-    this.filterYear = year;
-    if(this.filterLaunch==undefined || this.filterLaunch==null || this.filterLand==undefined ||  this.filterLand==null ||  this.filterLand=="")    
-      this.defaultResponse = this.dataBackup.filter((x:any)=>{ return x.launch_year==parseInt(year)});
-    else{
-      this.filterCombine();
+  SearchBy(){
+    
+    this.defaultResponse=[];
+    this.defaultResponse = this.dataBackup.filter((obj:any) => this.srch_repo !="" ? obj.name.toLowerCase() == this.srch_repo.toLowerCase() :null 
+    && this.srch_owner!="" ? obj.owner.login.toLowerCase() == this.srch_owner.toLowerCase() :null)
+    if(this.srch_issuecount!=null){
+      this.defaultResponse = this.dataBackup.filter((x:any)=>x.open_issues_count == this.srch_issuecount)
+    }
+    if(this.srch_watch!=null){
+      this.defaultResponse = this.dataBackup.filter((x:any)=>x.watchers_count == this.srch_watch)
+    }
+    if(this.srch_star!=null){
+      this.defaultResponse = this.dataBackup.filter((x:any)=>x.stargazers_count == this.srch_star)
     }
   }
-  filterByLaunch(data:any){
-    this.filterLaunch = data;
-    if(this.filterLand==undefined ||  this.filterLand==null ||  this.filterLand=="")    
-      this.defaultResponse = this.dataBackup.filter((x:any)=>{ return x.launch_success==data});
-    else{
-      this.filterCombine();
-    }
-  }
-  filterByLand(data:any){
-    this.filterLand = data;
-    if(this.filterLaunch==undefined ||  this.filterLaunch==null ||  this.filterLaunch=="")    
-      this.defaultResponse = this.dataBackup.filter((x:any)=>{ return x.rocket.first_stage.cores[0].land_success==data});
-    else{
-      this.filterCombine();
-    }
-  }
-  filterCombine(){
-    let url="";
-    // if(this.filterYear!=null && this.filterYear!=undefined){
-    //   url = "https://api.spacexdata.com/v3/launches?limit=100&launch_success=true&land_success=true&launch_year="+parseInt(this.filterYear);
-    //   this.getData(url);
-    // }
-    if(this.filterLaunch!=null && this.filterLand!=null){
-      url = "https://api.spaceXdata.com/v3/launches?limit=100&launch_success="+this.filterLaunch+"&land_success="+this.filterLand+"";
-      this.getData(url);
-    }
-    if(this.filterLand !=null && this.filterLand !=undefined && this.filterLaunch !=null && this.filterLaunch !=undefined && this.filterYear!=null && this.filterYear!=undefined){
-      url = "https://api.spacexdata.com/v3/launches?limit=100&launch_success="+this.filterLaunch+"&land_success="+this.filterLand+"&launch_year="+parseInt(this.filterYear)+"";
-      this.getData(url);
-    }
+  ClearSearch(){
+    this.srch_issuecount;
+    this.srch_owner="";
+    this.srch_repo="";
+    this.srch_star;
+    this.srch_watch;
+    this.Submit();
   }
   getData(url:any){
     this.http.get<any>(url).subscribe(data => {
